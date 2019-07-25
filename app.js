75ee29b5269edb6f95ee496f559dc154b2875892
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-07-25 17:54:57 -0600

    added SetTimer type of Break and Session

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const Header = () => <h1>Pomodoro Clock</h1>
 
+const SetTimer = ({ type }) => (
+    <div>
+        <div id={`${type}-label`}>{`${type} Length`}></div>
+    </div>
+)
 class App extends React.Component {
     render() {
         return(
             <div>
                 <Header/>
+                <div>
+                    <SetTimer type='Break' />
+                    <SetTimer type='Session' />
+                </div>
             </div>
+
         )
     }
 }
