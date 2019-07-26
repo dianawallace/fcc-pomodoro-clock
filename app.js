127630f355fd0b8adad5c5e313412c850f7fa17f
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-07-25 18:01:35 -0600

    added buttons for decrement and increment

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const Header = () => <h1>Pomodoro Clock</h1>
 
 const SetTimer = ({ type }) => (
     <div>
-        <div id={`${type}-label`}>{`${type} Length`}></div>
+        <div id={`${type}-label`}>{`${type} Length`}></div>        
+        <div>
+            <button id={`${type}-decrement`}></button>
+            <div></div>
+            <button id={`${type}-increment`}></button>
+        </div>
     </div>
 )
 class App extends React.Component {
